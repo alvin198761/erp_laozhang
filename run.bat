b5bd3b5359e24f518591b7c6656a00fec59d6680
tangzhichao <alvin198761@163.com>
2019-01-25 10:26:29 +0800

    增加执行脚本

---
+++ run.bat
@@ -0,0 +1,5 @@
+net start mysql80
+start java -jar erp_laozhang.jar
+echo '--------------------'
+TIMEOUT /T 5
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:9092
